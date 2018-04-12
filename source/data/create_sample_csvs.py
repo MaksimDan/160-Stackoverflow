25f32cc76b5b8cf1c3018adc5de699677109f2f6
MaksimDan <damaksimovich@ucdavis.edu>
2018-04-12 16:24:38 -0700

    working with data sxtraction

---
+++ source/data/create_sample_csvs.py
@@ -0,0 +1,29 @@
+import sqlite3
+import os
+
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+
+cursor = connection.cursor()
+table_names = ['badges', 'comments', 'posts', 'posthistory', 'postlinks', 'users', 'votes', 'tags']
+create_tables = [
+                 f'CREATE TABLE {table_names[0]} (UserId int, Name text, Date text)',
+                 f'CREATE TABLE {table_names[7]} (Id int, TagName text, Count int, ExcerptPostId int, WikiPostId int)'             
+                 f'CREATE TABLE {table_names[1]} (Id int, PostId int, Score int, Text text, CreationDate text, UserId int)'
+                 f'CREATE TABLE {table_names[2]} (UserId int, Name text, Date text)'
+                 f'CREATE TABLE {table_names[3]} (UserId int, Name text, Date text)'
+                 f'CREATE TABLE {table_names[4]} (UserId int, Name text, Date text)'
+                 f'CREATE TABLE {table_names[5]} (UserId int, Name text, Date text)'
+                 f'CREATE TABLE {table_names[6]} (UserId int, Name text, Date text)'
+                ]
+
+for name, create in zip(table_names, create_tables):
+    connection = sqlite3.connect(DIR_PATH + f'\\{name}.db')
+    cursor.execute(create)
+    connection.commit()
+    
+connection.close()
+
+# thoughts
+# work with reasonably random sample from all stackoverflow data
+#   have these written as a csv
+#   later work with sqlite databases for the complete files
---
+++ source/data/dl.py
@@ -0,0 +1,20 @@
+import requests
+import progressbar
+
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+    return local_filename
+
+
+base = 'https://archive.org'
+bar = progressbar.ProgressBar()
+with open('urls', 'r') as f:
+    contents = f.readlines()[0].split()
+    for url in bar(contents):
+        download_file(base + url)
